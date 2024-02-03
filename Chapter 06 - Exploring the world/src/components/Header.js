5ae437226411cb4d5aee72e571fe8f57ec9b02e0
abckyadav <anu250867anu@gmail.com>
2024-02-03 11:24:14 +0530

    chapter 6

---
+++ Chapter 06 - Exploring the world/src/components/Header.js
@@ -0,0 +1,21 @@
+import Logo from "../assets/logo.jpg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src={Logo} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
