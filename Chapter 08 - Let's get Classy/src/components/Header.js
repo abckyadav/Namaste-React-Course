28871894e3691f8b5a41e641287baba04e7b9f15
abckyadav <anu250867anu@gmail.com>
2024-02-06 12:38:35 +0530

    chapter 8

---
+++ Chapter 08 - Let's get Classy/src/components/Header.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Logo from "../assets/logo.jpg";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [loginLogout, setLoginLogout] = useState("Login");
+
+  const handleLoginLogout = () => {
+    {
+      loginLogout === "Login"
+        ? setLoginLogout("Logout")
+        : setLoginLogout("Login");
+    }
+  };
+  return (
+    <div className="header">
+      <div className="header-wrapper">
+        <Link to="/">
+          <div className="logo">
+            <img src={Logo} />
+          </div>
+        </Link>
+        <div className="nav-items">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <li>Cart</li>
+          </ul>
+        </div>
+        <div>
+          <button className="login-btn" onClick={handleLoginLogout}>
+            {loginLogout}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ Chapter 08 - Let's get Classy/src/utils/contants.js
@@ -0,0 +1,17 @@
+export const IMG_CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+// Image CDN URL for Restaurant Menu
+export const ITEM_IMG_CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
+
+export const SWIGGY_API =
+  "https://foodfire.onrender.com/api/restaurants?lat=24.90020700&lng=84.18107700&page_type=DESKTOP_WEB_LISTING";
+export const SWIGGY_MENU_API =
+  "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=24.90020700&lng=84.18107700&&submitAction=ENTER&restaurantId=";
+
+// menu items api card type key
+export const MENU_ITEM_TYPE_KEY =
+  "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
+export const RESTAURANT_TYPE_KEY =
+  "type.googleapis.com/swiggy.presentation.food.v2.Restaurant";
